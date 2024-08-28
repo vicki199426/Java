d1dc9eb141bcfba12dbf547c82206d0d26823944
vicki199426 <vmvicky1994@gmail.com>
2024-08-28 15:01:23 +0530

    first commit

---
+++ Test.java
@@ -0,0 +1,31 @@
+class Test{
+
+
+public static void main(String[] args){
+    int price=419;
+    float rating=3.7;
+    String brand_Name="Peter England";
+    String[] size={"S","M","L","XL","XXL"};
+    byte b=10;
+    short s=100;
+    int price=49000;
+    long mobile=9787316896l;
+    float esal1=45000.45f;
+    double esal2=45000.45;
+    double rating=4.5;
+    char avail='y';
+    boolean discount=true;
+    System.out.println(b);
+    System.out.println(s);
+    System.out.println(price);
+    System.out.println(mobile);
+    System.out.println(esal1);
+    System.out.println(esal2);
+    System.out.println(rating);
+    System.out.println(avail);
+    System.out.println(discount);
+    
+
+}
+
+}
